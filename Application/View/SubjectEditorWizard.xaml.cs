0724c959c4385e91441ac6ceed4e303839dc76a2
master <derdaster@gmail.com>
2016-02-04 19:13:19 +0100

    GUI wizard

--- Application/View/SubjectEditorWizard.xaml.cs
+++ Application/View/SubjectEditorWizard.xaml.cs
@@ -10,25 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SubjectEditorWizard.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SubjectEditorWizard : Window
     {
-        public MainWindow()
+        public SubjectEditorWizard()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var win = new SubjectEditorWizard();
-            win.ShowDialog();
-        }
     }
 }
